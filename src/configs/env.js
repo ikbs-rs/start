98d0d93b380432e5c3b45ae475342fe902946871
ikbs-rs <ikbs.rs@gmail.com>
2023-11-26 20:48:22 +0100

    ...

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,12 +1,14 @@
 const env = {
-    START_URL: 'https://82.117.213.106/start/',
-    ADM_URL: 'https://82.117.213.106/adm/',
-    CMN_URL: 'https://82.117.213.106/cmn/',
-    TIC_URL: 'https://82.117.213.106/tic/',
-    JWT_BACK_URL: 'https://82.117.213.106/badm',
-    ADM_BACK_URL: 'https://82.117.213.106/badm',
-    CMN_BACK_URL: 'https://82.117.213.106/bcmn',
-    TIC_BACK_URL: 'https://82.117.213.106/btic',
+    DOMEN: 'http://192.168.72.96',
+    START_URL: 'http://192.168.72.96/start/',
+    ADM_URL: 'http://192.168.72.96/adm/',
+    CMN_URL: 'http://192.168.72.96/cmn/',
+    TIC_URL: 'http://192.168.72.96/tic/',
+    JWT_BACK_URL: 'http://192.168.72.96/badm',
+    ADM_BACK_URL: 'http://192.168.72.96/badm',
+    CMN_BACK_URL: 'http://192.168.72.96/bcmn',
+    TIC_BACK_URL: 'http://192.168.72.96/btic',
+    IMG_BACK_URL: 'http://192.168.72.96/img',
   };
   
   export default env;
