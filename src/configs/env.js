414549432411b0a467b3fc6f3e443a20b6dee8e2
ikbs-rs <ikbs.rs@gmail.com>
2024-10-08 06:36:41 +0200

    ...

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,13 +1,13 @@
 const env = {
-    START_URL: 'https://172.16.26.245/start/',
-    ADM_URL: 'https://172.16.26.245/adm/',
-    CMN_URL: 'https://172.16.26.245/cmn/',
-    TIC_URL: 'https://172.16.26.245/tic/',
-    JWT_BACK_URL: 'https://172.16.26.245/badm',
-    ADM_BACK_URL: 'https://172.16.26.245/badm',
-    CMN_BACK_URL: 'https://172.16.26.245/bcmn',
-    TIC_BACK_URL: 'https://172.16.26.245/btic',
-    IMG_BACK_URL: 'https://172.16.26.245/img',
+    START_URL: 'https://82.117.213.106/start/',
+    ADM_URL: 'https://82.117.213.106/adm/',
+    CMN_URL: 'https://82.117.213.106/cmn/',
+    TIC_URL: 'https://82.117.213.106/tic/',
+    JWT_BACK_URL: 'https://82.117.213.106/badm',
+    ADM_BACK_URL: 'https://82.117.213.106/badm',
+    CMN_BACK_URL: 'https://82.117.213.106/bcmn',
+    TIC_BACK_URL: 'https://82.117.213.106/btic',
+    IMG_BACK_URL: 'https://82.117.213.106/img',
     REACT_APP_SITE_KEY: '6LcMQYQpAAAAALa_NZ9gNPllzWee1zuX5oSk6V-t',
   };
   
