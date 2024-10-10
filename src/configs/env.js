7999f305aaa6a3ac968b266f28060ca095b3c8d5
root <root@noname.localdomain>
2024-10-10 21:06:55 +0200

    ...

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,13 +1,13 @@
 const env = {
-    START_URL: 'https://82.117.213.106/start/',
-    ADM_URL: 'https://82.117.213.106/adm/',
-    CMN_URL: 'https://82.117.213.106/cmn/',
-    TIC_URL: 'https://82.117.213.106/tic/',
-    JWT_BACK_URL: 'https://82.117.213.106/badm',
-    ADM_BACK_URL: 'https://82.117.213.106/badm',
-    CMN_BACK_URL: 'https://82.117.213.106/bcmn',
-    TIC_BACK_URL: 'https://82.117.213.106/btic',
-    IMG_BACK_URL: 'https://82.117.213.106/img',
+    START_URL: 'https://188.93.122.138/start/',
+    ADM_URL: 'https://188.93.122.138/adm/',
+    CMN_URL: 'https://188.93.122.138/cmn/',
+    TIC_URL: 'https://188.93.122.138/tic/',
+    JWT_BACK_URL: 'https://188.93.122.138/badm',
+    ADM_BACK_URL: 'https://188.93.122.138/badm',
+    CMN_BACK_URL: 'https://188.93.122.138/bcmn',
+    TIC_BACK_URL: 'https://188.93.122.138/btic',
+    IMG_BACK_URL: 'https://188.93.122.138/img',
     REACT_APP_SITE_KEY: '6LcMQYQpAAAAALa_NZ9gNPllzWee1zuX5oSk6V-t',
   };
   
